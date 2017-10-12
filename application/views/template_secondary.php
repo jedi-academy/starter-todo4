35acb85f7107fcf2947d569a2fbe0c7b02f0b19a
Gisu Kim <sksmszhdzk@gmail.com>
2017-10-12 16:00:53 -0700

    put back menubar

--- application/views/template_secondary.php
+++ application/views/template_secondary.php
@@ -9,15 +9,28 @@
         <link rel="stylesheet" type="text/css" href="/assets/css/default.css"/>
 	</head>
 	<body>
-        <div id="content">
-            <h1>{pagetitle}</h1>
-            <div class="row">
-                <div class="col-lg-6 col-md-12">
-                    {leftside}
+
+        <div class="container">
+            <div class="navbar">
+                <div class="navbar-inner">
+                    {menubar} 
                 </div>
-                <div class="col-lg-6 col-md-12">
-                    {rightside}
+            </div>           
+            <div id="content">
+                <div id="content">
+                <h1>{pagetitle}</h1>
+                <div class="row">
+                    <div class="col-lg-6 col-md-12">
+                        {leftside}
+                    </div>
+                    <div class="col-lg-6 col-md-12">
+                        {rightside}
+                    </div>
                 </div>
+            </div>
+            </div>
+            <div id="footer" class="span12">
+                Copyright &copy; 2017,  <a href="mailto:someone@somewhere.com">Me</a>.
             </div>
         </div>
         <script src="/assets/js/jquery-1.11.1.min.js"></script>
