52f2696d6e330df589d3b0ab846664946c55909b
unhouseledutraquist <andrewlbusto@gmail.com>
2017-10-15 12:03:53 -0700

    task quantity appropriately listed, task list on the page but not properly implemented.

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,8 +4,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends Application
 {
+        /**
+         * Constructs an instance of the Welcome class.
+         * 
+         * Initializes $this->data['remaining_tasks']
+         *  to the appropriate number of tasks.
+         */
+        function __construct()
+        {
+            parent::__construct();
+            
+            $tasks = $this->tasks->all();   // get all the tasks
 
-	/**
+            // count how many are not done
+            $count = 0;
+            foreach($tasks as $task) {
+                    if ($task->status != 2) $count++;
+            }
+            // and save that as a view parameter
+            $this->data['remaining_tasks'] = $count;
+        }
+         /**
 	 * Index Page for this controller.
 	 *
 	 * Maps to the following URL
@@ -19,8 +38,22 @@ class Welcome extends Application
 	 */
 	public function index()
 	{
-		$this->data['pagebody'] = 'homepage';
-		$this->render(); 
+            $this->data['pagebody'] = 'homepage';
+            $this->render(); 
 	}
+        
+        public function display_tasks()
+        {
+            $tasks = $this->tasks->all();   // get all the tasks
+            
+            // process the array in reverse, until we have five
+            $count = 0;
+            foreach(array_reverse($tasks) as $task) {
+                $display_tasks[] = (array) $task;
+                $count++;
+                if ($count >= 5) break;
+            }
+            $this->data['display_tasks'] = $display_tasks;
+        }
 
 }
