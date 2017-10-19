95d233c486271f0f16fe19da1021296f30a373e5
Gisu Kim <sksmszhdzk@gmail.com>
2017-10-19 14:18:37 -0700

    clear stage 7

--- application/views/itemnav.php
+++ application/views/itemnav.php
@@ -1,12 +1,3 @@
-{pagination}
-<table class="table">
-        <tr>
-                <th>Id</th>
-                <th>Task</th>
-                <th>Status</th>
-        </tr>
-        {display_tasks}
-</table>
 <ul class="nav nav-pills">
         <li><a href="/mtce/page/{first}">First</a></li>
         <li><a href="/mtce/page/{previous}">Previous</a></li>
