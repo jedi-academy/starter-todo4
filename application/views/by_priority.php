5e822a11cdb96e56c653fc59739bd2679ad8dd3a
Gisu Kim <sksmszhdzk@gmail.com>
2017-10-19 13:47:58 -0700

    clear stage 4

--- application/views/by_priority.php
+++ application/views/by_priority.php
@@ -1,15 +1,15 @@
 <h3>Tasks by Priority</h3>
 <table class="table">
-    <tr>
-        <th>Id</th>
-        <th>Task</th>
-        <th>Priority</th>
-    </tr>
-    {display_tasks}
-    <tr>
-        <td>{id}</td>
-        <td>{task}</td>
-        <td>{priority}</td>
-    </tr>
-    {/display_tasks}    
+        <tr>
+                <th>Id</th>
+                <th>Task</th>
+                <th>Status</th>
+        </tr>
+        {display_tasks}
+        <tr>
+                <td>{id}</td>
+                <td>{task}</td>
+                <td>{status}</td>
+        </tr>
+        {/display_tasks}    
 </table>
