cc57b631f169357f391a94d85e372d9a86129ca9
Ebon Chung <jay0000015chung@gmail.com>
2017-10-19 14:27:32 -0700

    Close #15

--- application/views/itemlist.php
+++ application/views/itemlist.php
@@ -7,11 +7,3 @@
         </tr>
         {display_tasks}
 </table>
-<table class="table">
-        <tr>
-                <th>Id</th>
-                <th>Task</th>
-                <th>Status</th>
-        </tr>
-        {display_tasks}
-</table>
