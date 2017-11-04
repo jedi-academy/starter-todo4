61fc925d6ff411777b46db07f87e7e2c77e2e4bf
Bryn Beaudry <babeaudry@gmail.com>
2017-11-04 02:31:54 -0700

    closes #29 First Unit test ever. Tests every test input with every funciton and prints to console. Command for running can be found in class. Code coverage report from issue 31 still needs to be generated

--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -9,38 +9,102 @@
 class TaskTest extends PHPUnit\Framework\TestCase
 {
     private $CI;
+
+    /**
+     * @bbeau
+     *
+     * command to run test from project root folder:
+     *
+     * /usr/local/bin/php ./vendor/bin/phpunit
+     *
+     * check your php is correctly configured by this command
+     * ls /usr/local/bin | grep php
+     *
+     * use one of the results listed or get linux
+     *
+     * confirm with php -v, in the bin dir
+     *
+     * Thats all you need.
+     * The original test was not very helpful, and would obviously pass with anything if you're using a magic method
+     *
+     * @bbeau
+     *
+     */
+
+
+    private $GLOBALS = array();
+
     private $task;
+    private $inputs;
+    private $failures;
+
     public function setUp()
     {
+
+
         // Load CI instance normally
         $this->CI =& get_instance();
         $this->task = new $this->CI->task;
+        $this->failures = array();
 
         if (isset($this->CI)) {
             $this->task = new $this->CI->task;
-        }else{
-            var_dump("no task");
+        } else {
+            print_r("no task");
         }
+        print_r($this->task);
 
-        var_dump($this->task);
+        $this->inputs = array(
+            'fV2MxxW36p7WX0bpCqr6yrJGSkhpMO1tqQudj6nOPjbfay7fJqhU9kUWyNuUEK6x' => "64 Characters && Length < 64",
+            'uyVCPdX6AU1kTo4EWyfup0ElEm8kqaKdh0vhDzskRFkjf5kdsDpwp0D8buYQvZ7' => "63 Characters && Length < 64",
+            'fV2MxxW36p7WX0bpCqr6yrJGSkhpMO1tqQudj6nOPjbfay7fJqhU9kUWyNuUEK6     ' => "63 Characters and +2 Whitespace && Length < 64 : Sanitizes whtspc",
+            'abc' => "Alphabet Characters",
+            123 => "Numbers",
+            3 => '3 @ < 4',
+            4 => '4 @ < 4',
+            -1 => 'Negative Number',
+        );
 
+        $this->testTaskSetMethods();
+
+        echo "FINISHED, all good";
     }
+
+
+
     public function testTaskSetMethods()
     {
-        $task = $this->task;
-        //exactly 64 chars desc
-        $this->assertEquals(true, $task->set='fV2MxxW36p7WX0bpCqr6yrJGSkhpMO1tqQudj6nOPjbfay7fJqhU9kUWyNuUEK6x', "able to set 64 chacters?");
-        /*
-        //exactly 63
-        $this->assertEquals(true, $this->task='uyVCPdX6AU1kTo4EWyfup0ElEm8kqaKdh0vhDzskRFkjf5kdsDpwp0D8buYQvZ7',"able to set 63 chars?");
-        //spaces are escaped
-        $this->assertEquals(true, $this->task='fV2MxxW36p7WX0bpCqr6yrJGSkhpMO1tqQudj6nOPjbfay7fJqhU9kUWyNuUEK6     ', "is whitespace sanitized?");
-        $this->assertEquals(true, $this->task='abc',"can set non numeric?");
-        $this->assertEquals(true, $this->task='123',"can set numeric?");
-        $this->assertEquals(true, $this->task=3,'can set 3?');
-        $this->assertEquals(true, $this->task=4, 'can set 4?');
-        $this->assertEquals(true, $this->task=-1,'can set negative?');
-        */
+        foreach ($this->task as $attr => $val) {
+            echo PHP_EOL;
+            echo '~'.strtoupper($attr).'~';
+            echo PHP_EOL;
 
+            foreach ($this->inputs as $input => $affrm) {
+                $isAcceptable = 1;
+                try {
+                    $attrStr = 'set'.ucfirst($attr); //name of the set fn
+                    $this->assertEquals(true, $this->task->$attrStr($input), "Description: $affrm\n");
+                } catch (PHPUnit\Framework\ExpectationFailedException $e) {
+                    array_push($this->failures,"Failed! : \nInput: $input\nFunction : $attrStr\n".$e->getMessage().PHP_EOL);
+                    $isAcceptable = 0;
+                }
+                if($isAcceptable) {
+                    echo PHP_EOL;
+                    echo "Passed! : \nInput: $input\nFunction : $attrStr \nDescription: $affrm" . PHP_EOL;
+                }
+            }
+            echo PHP_EOL;
+        }
+        echo PHP_EOL;
+        if(!empty($this->failures))
+        {
+            throw new PHPUnit\Framework\ExpectationFailedException(
+                "_______________FAILURES_________________\n\t".implode("\n\t", $this->failures)."\n\t". count($this->failures)." assertions failed!"
+            );
+        }
     }
+
+
+
+
 }
