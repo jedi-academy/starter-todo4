fe6f01bb421616ad152f3efb94186dbb5b0f11af
unhouseledutraquist <andrewlbusto@gmail.com>
2017-11-04 16:53:13 -0700

    closes #31

---
+++ tests/TaskTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Description of TaskTest
+ *
+ * @author Andrew
+ */
+class TaskTest extends PHPUnit_Framework_TestCase {
+
+    private $task;
+
+    public function setUp() 
+    {
+        $this->task = new Task();
+    }
+
+    public function testSetValid() 
+    {
+        $task = $this->task->setTask("Do stuff");
+        $size = $this->task->setSize(10);
+        $priority = $this->task->setPriority(1);
+        $group = $this->task->setGroup(2);
+        
+        $this->assertEquals("Do stuff", $task);
+        $this->assertEquals(10, $size);
+        $this->assertEquals(1, $priority);
+        $this->assertEquals(2, $group);
+    }
+    
+    public function testSetInvalid()
+    {
+        $task = $this->task->setTask(20);
+        $size = $this->task->setSize(-100);
+        $priority = $this->task->setPriority(100);
+        $group = $this->task->setGroup(200);
+        
+        $this->assertNotEquals(20, $task);
+        $this->assertNotEquals(-100, $size);
+        $this->assertNotEquals(100, $priority);
+        $this->assertNotEquals(200, $group);
+    }
+}
