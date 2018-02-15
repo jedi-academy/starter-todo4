2b05bf79125e7b9055aaaa6f2fb3b6db2552c92a
Tliyen <liyan.tong@gmail.com>
2018-02-15 15:11:33 -0800

    Create table column names

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,1 +1,8 @@
 <div class="alert alert-info">{remaining_tasks} tasks are left to do!</div>
+<table class="table">
+    <tr>
+        <th>Id</th>
+        <th>Task</th>
+        <th>Priority</th>
+    </tr>
+</table>
