db944a3d23c2ee77d0c9bb0a545c27750a3b8f9a
Jeffrey Chou <jeffreychou1991@gmail.com>
2018-02-16 09:35:12 -0800

    Updated controller/views.php to grab tasks by priority and send them to the view

--- application/controllers/Views.php
+++ application/controllers/Views.php
@@ -12,9 +12,47 @@ class Views extends Application
         $tasks = $this->tasks->all();   // get all the tasks
         $this->data['content'] = 'Ok'; // so we don't need pagebody
         $this->data['leftside'] = $this->makePrioritizedPanel($tasks);
-        $this->data['rightside'] = $this->makeCategorizedPanel($tasks);
+        //$this->data['rightside'] = $this->makeCategorizedPanel($tasks);
 
         $this->render('template_secondary'); 
+    }
+
+    /**
+     * Grabs all of the unfinished tasks, order 
+     * them by high to low priority and returns them
+     * @param $tasks to tasks list
+     */
+    function makePrioritizedPanel($tasks) {
+
+         // extract the undone tasks
+        foreach ($tasks as $task)
+        {
+            if ($task->status != 2)
+                $undone[] = $task;
+        }
+        usort($undone, array("Views","orderByPriority"));
+        // substitute the priority name
+        foreach ($undone as $task)
+            $task->priority = $this->app->priority($task->priority);
+
+        // convert the array of task objects into an array of associative objects       
+        foreach ($undone as $task)
+            $converted[] = (array) $task;
+
+        // and then pass them on
+        $parms = ['display_tasks' => $converted];
+        return $this->parser->parse('by_priority', $parms, true);
+    }
+
+    // return -1, 0, or 1 of $a's priority is higher, equal to, or lower than $b's
+    function orderByPriority($a, $b)
+    {
+        if ($a->priority > $b->priority)
+            return -1;
+        elseif ($a->priority < $b->priority)
+            return 1;
+        else
+            return 0;
     }
 
 }
