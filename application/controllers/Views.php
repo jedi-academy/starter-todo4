6cfc525b27901b32b0d4174dc2bcc4252cf67994
Geoff McLennan <geoff.d.mclennan@gmail.com>
2018-02-15 16:11:32 -0800

    Added sorting by priority and category

--- application/controllers/Views.php
+++ application/controllers/Views.php
@@ -9,9 +9,40 @@ class Views extends Application
         $this->data['pagetitle'] = 'Ordered TODO List';
         $tasks = $this->tasks->all();
         $this->data['content'] = "Ok";
-        $this->data['leftside'] = 'by_priority';
-        $this->data['rightside'] = 'by_category';
+        $this->data['leftside'] = $this->makePrioritizedPanel($tasks);
+        $this->data['rightside'] = $this->makeCategorizedPanel($tasks);
 
         $this->render('template_secondary');
     }
+
+    function makePrioritizedPanel($tasks)
+    {
+        foreach ($tasks as $task)
+        {
+            if ($task->status != 2)
+                $undone[] = $task;
+        }
+
+        usort($undone, "orderByPriority");
+
+        foreach ($undone as $task)
+        {
+            $task->priority = $this->app->priority($task->priority);
+            $converted[] = (array) $task;
+        }
+
+        $params = ['display_tasks' => $converted];
+        return $this->parser->parse('by_priority', $params, true);
+    }
+
+    function makeCategorizedPanel($tasks)
+    {
+        $params = ['display_tasks' => $this->tasks->getCategorizedTasks()];
+        return $this->parser->parse('by_category', $params, true);
+    }
+}
+
+function orderByPriority($a, $b)
+{
+    return $b->priority - $a->priority;
 }
