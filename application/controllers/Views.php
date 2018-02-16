8886740a77b2b7149d15147c7f4ea2f466f82a03
Jeffrey Chou <jeffreychou1991@gmail.com>
2018-02-15 16:22:39 -0800

    Added by_category.php view and by_priority.php view. Updated Views.php controller to call functions for priority and category

--- application/controllers/Views.php
+++ application/controllers/Views.php
@@ -11,8 +11,8 @@ class Views extends Application
         $this->data['pagetitle'] = 'Ordered TODO List';
         $tasks = $this->tasks->all();   // get all the tasks
         $this->data['content'] = 'Ok'; // so we don't need pagebody
-        $this->data['leftside'] = 'by_priority';
-        $this->data['rightside'] = 'by_category';
+        $this->data['leftside'] = $this->makePrioritizedPanel($tasks);
+        $this->data['rightside'] = $this->makeCategorizedPanel($tasks);
 
         $this->render('template_secondary'); 
     }
