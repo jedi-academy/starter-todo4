29d53fb1c5d82432b8a88f1c4a8b128482306d42
Daniel <daniel_katz@hotmail.ca>
2018-02-16 16:42:31 -0800

    Job 3 Progress made.
    Signed-off-by: Daniel <daniel_katz@hotmail.ca>

--- application/views/template_secondary.php
+++ application/views/template_secondary.php
@@ -15,10 +15,17 @@
                     {menubar}
 				</div>
             </div>
-            <div id="content">
-                <h1>{pagetitle}</h1>
-                {content}
-            </div>
+						<div id="content">
+    <h1>{pagetitle}</h1>
+    <div class="row">
+        <div class="col-lg-6 col-md-12">
+            {leftside}
+        </div>
+        <div class="col-lg-6 col-md-12">
+            {rightside}
+        </div>
+    </div>
+</div>
             <div id="footer" class="span12">
                 Copyright &copy; 2018,  <a href="mailto:someone@somewhere.com">Me</a>.
             </div>
