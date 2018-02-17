0d71447ea0465423833a851385261fa2edc7950d
Daniel <daniel_katz@hotmail.ca>
2018-02-16 16:45:35 -0800

    Syncing attempt
    Signed-off-by: Daniel <daniel_katz@hotmail.ca>

---
+++ application/views/homepage.php
@@ -0,0 +1,16 @@
+<div class="alert alert-info">{remaining_tasks} tasks are left to do!</div>
+
+<table class="table">
+    <tr>
+        <th>Id</th>
+        <th>Task</th>
+        <th>Priority</th>
+    </tr>
+{display_tasks}
+    <tr>
+        <th>{id}</th>
+        <th>{task}</th>
+        <th>{priority}</th>
+    </tr>
+{/display_tasks}
+</table>
