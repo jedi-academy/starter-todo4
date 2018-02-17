9e823faf9ea8016ee77106b7033ea3997f45a366
Tliyen <liyan.tong@gmail.com>
2018-02-16 17:37:58 -0800

    Seperate dislpaying items in itemlist.php to oneitem.php

--- application/views/itemlist.php
+++ application/views/itemlist.php
@@ -5,10 +5,4 @@
                 <th>Status</th>
         </tr>
         {display_tasks}
-        <tr>
-            <td>{id}</td>
-            <td>{task}</td>
-            <td>{status}</td>
-        </tr>
-        {/display_tasks}
 </table>
