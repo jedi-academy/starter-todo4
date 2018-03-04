055c8ceffe99075125f7f7c3d02e77f15a8ed161
Tliyen <liyan.tong@gmail.com>
2018-03-03 17:42:33 -0800

    Seperate page display into a show_page function

--- application/controllers/Mtce.php
+++ application/controllers/Mtce.php
@@ -4,25 +4,29 @@ class Mtce extends Application {
 
   private $items_per_page = 10;
 
-        public function index()
-        {
-                $this->data['pagetitle'] = 'TODO List Maintenance';
-                $tasks = $this->tasks->all(); // get all the tasks
+  public function index()
+  {
+    $tasks = $this->tasks->all(); // get all the tasks
+    $this->show_page($tasks);
+  }
 
-                // substitute the status name
-                foreach ($tasks as $task)
-                        if (!empty($task->status))
-                                                $task->status = $this->app->status($task->status);
+  // Show a single page of todo items
+  private function show_page($tasks)
+  {
+    $this->data['pagetitle'] = 'TODO List Maintenance';
+    // build the task presentation output
+    $result = ''; // start with an empty array      
+    foreach ($tasks as $task)
+    {
+      if (!empty($task->status))
+      $task->status = $this->app->status($task->status);
+      $result .= $this->parser->parse('oneitem', (array) $task, true);
+    }
+    $this->data['display_tasks'] = $result;
 
-                // build the task presentation output
-                $result = '';   // start with an empty array
-                foreach ($tasks as $task)
-                        $result .= $this->parser->parse('oneitem',(array)$task,true);
-
-                // and then pass them on
-                $this->data['display_tasks'] = $result;
-                $this->data['pagebody'] = 'itemlist';
-                $this->render();
-        }
+    // and then pass them on
+    $this->data['pagebody'] = 'itemlist';
+    $this->render();
+  }
 
 }
