9c08f7454fef364b0cac52c7d543c28fddcfcd2c
Jeffrey Chou <jeffreychou1991@gmail.com>
2018-03-04 13:06:46 -0800

    updated controllers/Mtce to be able to delete tasks. Updated views/itemedit to add task deletion button

--- application/controllers/Mtce.php
+++ application/controllers/Mtce.php
@@ -155,4 +155,14 @@ class Mtce extends Application {
     redirect('/mtce');
   }
 
+  // Delete this item altogether
+  function delete()
+  {
+      $dto = $this->session->userdata('task');
+      $task = $this->tasks->get($dto->id);
+      $this->tasks->delete($task->id);
+      $this->session->unset_userdata('task');
+      redirect('/mtce');
+  }
+
 }
--- application/views/itemedit.php
+++ application/views/itemedit.php
@@ -4,5 +4,6 @@
     {fpriority}
     {zsubmit}
     <a href="/mtce/cancel"><input type="button" value="Cancel the current edit"/></a>
+    <a href="/mtce/delete"><input type="button" value="Delete this todo item"/></a>
 </form>
     {error}
