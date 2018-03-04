0412e4c1ce0b5cae2734fc3ef1ab9770883edf81
Tliyen <liyan.tong@gmail.com>
2018-03-03 20:32:40 -0800

    Update complete method to complete tasks

--- application/controllers/Views.php
+++ application/controllers/Views.php
@@ -1,84 +1,91 @@
 <?php
 
 /**
- * Views controller
- */
+* Views controller
+*/
 class Views extends Application
 {
 
-    public function index()
+  public function index()
+  {
+    $this->data['pagetitle'] = 'Ordered TODO List';
+    $tasks = $this->tasks->all();   // get all the tasks
+    $this->data['content'] = 'Ok'; // so we don't need pagebody
+    $this->data['leftside'] = $this->makePrioritizedPanel($tasks);
+    $this->data['rightside'] = $this->makeCategorizedPanel($tasks);
+
+    $this->render('template_secondary');
+  }
+
+  /**
+  * Grabs all of the unfinished tasks, order
+  * them by high to low priority and returns them
+  * @param $tasks to tasks list
+  */
+  function makePrioritizedPanel($tasks) {
+
+    // extract the undone tasks
+    foreach ($tasks as $task)
     {
-        $this->data['pagetitle'] = 'Ordered TODO List';
-        $tasks = $this->tasks->all();   // get all the tasks
-        $this->data['content'] = 'Ok'; // so we don't need pagebody
-        $this->data['leftside'] = $this->makePrioritizedPanel($tasks);
-        $this->data['rightside'] = $this->makeCategorizedPanel($tasks);
-
-        $this->render('template_secondary');
+      if ($task->status != 2)
+      $undone[] = $task;
     }
+    usort($undone, array("Views","orderByPriority"));
+    // substitute the priority name
+    foreach ($undone as $task)
+    $task->priority = $this->app->priority($task->priority);
 
-    /**
-     * Grabs all of the unfinished tasks, order
-     * them by high to low priority and returns them
-     * @param $tasks to tasks list
-     */
-    function makePrioritizedPanel($tasks) {
+    // convert the array of task objects into an array of associative objects
+    foreach ($undone as $task)
+    $converted[] = (array) $task;
 
-         // extract the undone tasks
-        foreach ($tasks as $task)
-        {
-            if ($task->status != 2)
-                $undone[] = $task;
-        }
-        usort($undone, array("Views","orderByPriority"));
-        // substitute the priority name
-        foreach ($undone as $task)
-            $task->priority = $this->app->priority($task->priority);
+    // and then pass them on
+    $parms = ['display_tasks' => $converted];
+    $role = $this->session->userdata('userrole');
+    $parms['completer'] = ($role == ROLE_OWNER) ? '/views/complete' : '#';
+    return $this->parser->parse('by_priority', $parms, true);
+  }
 
-        // convert the array of task objects into an array of associative objects
-        foreach ($undone as $task)
-            $converted[] = (array) $task;
+  // return -1, 0, or 1 of $a's priority is higher, equal to, or lower than $b's
+  function orderByPriority($a, $b)
+  {
+    if ($a->priority > $b->priority)
+    return -1;
+    elseif ($a->priority < $b->priority)
+    return 1;
+    else
+    return 0;
+  }
 
-        // and then pass them on
-        $parms = ['display_tasks' => $converted];
-        $role = $this->session->userdata('userrole');
-        $parms['completer'] = ($role == ROLE_OWNER) ? '/views/complete' : '#';
-        return $this->parser->parse('by_priority', $parms, true);
+  /**
+  * Uses models/Tasks.php getCategorizedTasks method to get a list of
+  * Tasks ordered by category
+  */
+  function makeCategorizedPanel($tasks)
+  {
+    $parms = ['display_tasks' => $this->tasks->getCategorizedTasks()];
+    $role = $this->session->userdata('userrole');
+    $parms['completer'] = ($role == ROLE_OWNER) ? '/views/complete' : '#';
+    return $this->parser->parse('by_category', $parms, true);
+  }
+
+  // complete flagged items
+  function complete() {
+    $role = $this->session->userdata('userrole');
+    if ($role != ROLE_OWNER) redirect('/work');
+
+    // loop over the post fields, looking for flagged tasks
+    foreach($this->input->post() as $key=>$value) {
+      if (substr($key,0,4) == 'task') {
+        // find the associated task
+        // MORE COMING HERE
+        $taskid = substr($key,4);
+        $task = $this->tasks->get($taskid);
+        $task->status = 2; // complete
+        $this->tasks->update($task);
+      }
     }
-
-    // return -1, 0, or 1 of $a's priority is higher, equal to, or lower than $b's
-    function orderByPriority($a, $b)
-    {
-        if ($a->priority > $b->priority)
-            return -1;
-        elseif ($a->priority < $b->priority)
-            return 1;
-        else
-            return 0;
-    }
-
-    /**
-     * Uses models/Tasks.php getCategorizedTasks method to get a list of
-     * Tasks ordered by category
-     */
-    function makeCategorizedPanel($tasks)
-    {
-        $parms = ['display_tasks' => $this->tasks->getCategorizedTasks()];
-        $role = $this->session->userdata('userrole');
-        $parms['completer'] = ($role == ROLE_OWNER) ? '/views/complete' : '#';
-        return $this->parser->parse('by_category', $parms, true);
-    }
-
-    // complete flagged items
-    function complete() {
-            // loop over the post fields, looking for flagged tasks
-            foreach($this->input->post() as $key=>$value) {
-                    if (substr($key,0,4) == 'task') {
-                            // find the associated task
-                            // MORE COMING HERE
-                    }
-            }
-            $this->index();
-    }
+    $this->index();
+  }
 
 }
