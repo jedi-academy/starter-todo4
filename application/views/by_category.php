c837a4eef18f5a85184a78909ab8d8b7adf42f5d
Tliyen <liyan.tong@gmail.com>
2018-03-03 20:26:08 -0800

    Update task lists to include checklist column and checkbox for completion

--- application/views/by_category.php
+++ application/views/by_category.php
@@ -3,12 +3,14 @@
   <table class="table">
     <tr>
       <th>Id</th>
+      <th></th>
       <th>Task</th>
       <th>Category</th>
     </tr>
     {display_tasks}
     <tr>
       <td>{id}</td>
+      <td><input type='checkbox' name='task{id}'/></td>
       <td>{task}</td>
       <td>{group}</td>
     </tr>
--- application/views/by_priority.php
+++ application/views/by_priority.php
@@ -3,12 +3,14 @@
   <table class="table">
     <tr>
       <th>Id</th>
+      <th></th>
       <th>Task</th>
       <th>Priority</th>
     </tr>
     {display_tasks}
     <tr>
       <td>{id}</td>
+      <td><input type='checkbox' name='task{id}'/></td>
       <td>{task}</td>
       <td>{priority}</td>
     </tr>
