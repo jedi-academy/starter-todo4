a457f8b2f0f8d1cbfa8602840bdbf3b4c0e0e1ab
Tliyen <liyan.tong@gmail.com>
2018-03-11 16:01:50 -0700

    Create simple test

--- tests/TaskListTest.php
+++ tests/TaskListTest.php
@@ -4,12 +4,22 @@ use PHPUnit\Framework\TestCase;
 
 class TaskListTest extends TestCase
 {
+  private $CI;
 
-        function setUp()
-        {
-                $this->CI = &get_instance();
-                $this->CI->load->model('tasks');
-                $this->item = new Tasks();
-        }
+  function setUp()
+  {
+    $this->CI = &get_instance();
+    $this->CI->load->model('tasks');
+    $this->item = new Tasks();
+  }
+
+  
+  // Check if task list is a valid size.
+  public function testListSize()
+  {
+    $completed = $this->tasks->getCompletedTask();
+    $uncompleted = $this->tasks->getUncompletedTask();
+    $this->assertLessThan($uncompleted, $completed);
+  }
 
 }
--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -5,11 +5,22 @@ use PHPUnit\Framework\TestCase;
 class TaskTest extends TestCase
 {
 
+  private $CI;
+
         function setUp()
         {
                 $this->CI = &get_instance();
                 $this->CI->load->model('task');
                 $this->item = new Task();
+        }
+
+        // Check if task name is longer than 30 characters
+        function testSetTask()
+        {
+            $expected = 'A Task name cannot be longer than 30 characters';
+            $this->task->setId(1);
+            $this->task->setTask($expected);
+            $this->assertEquals($expected, $this->task->task);
         }
 
 }
